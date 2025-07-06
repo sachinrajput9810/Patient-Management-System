cfa6db21fc6051f96d3b74605a8ddef62bafe2b5
sachinrajput9810 <rajputsachin9810@gmail.com>
2025-07-06 23:13:34 +0530

    Registered billing-service as GRPC service

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,31 @@
+package com.pm.billingservice.grpc;
+
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@GrpcService
+public class BillingGrpcService  extends BillingServiceImplBase {
+
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(BillingRequest request, StreamObserver<BillingResponse> responseObserver){
+        log.info("createBillingAccount Request Received {}", request.toString());
+         // Business logic to perform calculation , DB operations etc
+        BillingResponse billingResponse = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(billingResponse);
+        responseObserver.onCompleted();
+    }
+
+}
